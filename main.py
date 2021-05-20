ade7deab2010015054051aac493ef83e3a06e081
Atheelah <atheelahvanderschyff17@gmail.com>
2021-05-20 15:56:01 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Basic Calculator
+
+class Add:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+
+    def subtract(self, a, b):
+        return a - b
+
+
+class Divide:
+    def __init__(self):
+        pass
+
+    def Divide(self, a, b):
+        return a / b
+
+
+class Multiply:
+    def __init__(self):
+        pass
+
+    def Multiply(self, a, b):
+        return a * b
